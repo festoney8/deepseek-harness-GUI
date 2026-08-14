8aca71eb7f91ff5758e26a4d8e3e28a07f187d44
festoney8 <festoney8@gmail.com>
2026-08-14 18:44:34 +0800

    fix: iframe permission

--- src/components/WebUiView.vue
+++ src/components/WebUiView.vue
@@ -6,5 +6,17 @@ const src = computed(() => `http://127.0.0.1:${props.port}/`);
 </script>
 
 <template>
-  <iframe :src="src" class="h-full w-full border-0 bg-white" />
+  <iframe
+    :src="src"
+    allow="
+      camera *;
+      clipboard-read *;
+      clipboard-write *;
+      downloads *;
+      fullscreen *;
+      microphone *;
+      speaker-selection *;
+    "
+    class="h-full w-full border-0 bg-white"
+  />
 </template>
