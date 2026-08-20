b9e71604d35c08ba15fb4f27568b58e3a560ce5a
festoney8 <festoney8@gmail.com>
2026-08-21 02:10:37 +0800

    feat: use nuxtui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import ui from "@nuxt/ui/vue-plugin";
 import App from "./App.vue";
 
 createApp(App).use(createPinia()).mount("#app");
+
+const app = createApp(App);
+app.use(ui);
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import ui from "@nuxt/ui/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ui({
+      ui: {
+        colors: {
+          primary: "sky",
+        },
+      },
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
